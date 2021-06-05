5e8f288edcfe485989bcf54459ca6e6a94626eed
JAzzad5 <carlosjosue0114@gmail.com>
2021-06-05 16:15:42 -0600

    [NEW] openacademy

---
+++ openacademy/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from . import controlers
+from . import models
+
---
+++ openacademy/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """Manage Trainigs""",
+
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "Carlos Quezada",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        #'security/ir.model.access.csv',
+        'views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
